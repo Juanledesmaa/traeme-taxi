1d150e4c10e643fa1d76df9059cbab6f40477211
Juan Ledesma <umbranito@gmail.com>
2024-06-21 01:01:22 -0400

    fix warnings

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -16,16 +16,15 @@ const Introduction = () => (
                         <h4 class="text-blk description">
                         En traemeTaxi, entendemos que necesitas más que solo un taxi. Necesitas un transporte confiable que hable tu idioma y al que puedas confiar para tu trabajo y más. Estamos aquí para llenar ese vacío que a menudo las grandes compañías descuidan. Ofrecemos seguridad, calidad, confianza y velocidad, todo a precios accesibles. Nuestros servicios incluyen una amplia gama de opciones, desde traslados locales y entregas hasta viajes entre estados y servicios por hora.
                         </h4>
-                        <a>
-                            <button class="explore">
-                                View More
-                            </button>
-                        </a>
+                        <button class="explore">
+                            View More
+                        </button>
+
                     </div>
                     <div class="videoContainer">
-                        <iframe allowfullscreen="allowfullscreen" class="mainVideo" controls="controls" src="https://www.youtube.com/embed/svg%3E?">
+                        <iframe title="Video-traeme-taxi" allowfullscreen="allowfullscreen" class="mainVideo" controls="controls" src="https://www.youtube.com/embed/svg%3E?">
                         </iframe>
-                        <img class="dotsImg image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/cw3.svg"></img>
+                        <img className="dotsImg image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/cw3.svg" alt="alternative"></img>
                     </div>
                 </div>
             </div>
